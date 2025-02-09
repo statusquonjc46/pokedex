7323d0d8989d4ff1e8df2596a81f71ff58725fd5
statusquonjc46 <nicholas.coletta@gmail.com>
2025-02-09 12:02:34 -0500

    initial creation of REPL cli

--- main.go
+++ main.go
@@ -1,14 +1,34 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func cleanInput(text string) []string {
-	return strings.Split(strings.ToLower(text), " ")
+	return strings.Fields(strings.ToLower(text))
 }
 
 func main() {
-	fmt.Println(cleanInput("Hello, World!"))
+	for {
+		scanner := bufio.NewScanner(os.Stdin)
+		fmt.Print("Pokedex > ")
+		for scanner.Scan() {
+			input := scanner.Text()
+
+			if input == "exit" {
+				os.Exit(1)
+			}
+
+			first := cleanInput(input)[0]
+			fmt.Printf("Your command was: %s\n", first)
+			fmt.Print("Pokedex > ")
+
+		}
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading from stdin:", err)
+		}
+	}
 }
